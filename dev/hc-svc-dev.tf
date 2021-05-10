07a9e7e8a344fe0461ef5f0a798a22bea78e5a45
Nick Schofield <nick.schofield@westpac.co.nz>
2021-05-10 16:39:12 +1200

    Add in resource group  module to svc workspace

--- dev/hc-svc-dev.tf
+++ dev/hc-svc-dev.tf
@@ -92,3 +92,17 @@ module "varArmTenantId-hc-svc-dev" {
 #   workspace_id = module.workspace-hc-svc-dev.tw-tw-main-id
 #   sensitive    = true
 # }
+
+
+module "example_resource_group" {
+  source  = "app.terraform.io/funkytown/resource-group/azurerm"
+  version = "1.0.0"
+  name        = "example-functionapp-rg"
+  location    = "australiaeast"
+  salary_id   = "m809808"
+  environment = "Sandbox"
+  app_owner   = "Nick Schofield"
+  app_id      = "ID1010"
+  squad_name  = "Cloud Compute"
+  squad_code  = "1"
+}
