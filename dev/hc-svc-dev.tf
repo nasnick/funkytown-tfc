4733bd89654d2526ab19b12e7c8a0cbed276b746
Nick Schofield <nick.schofield@westpac.co.nz>
2021-05-11 15:51:38 +1200

    Bump workspace version for svc workspace

--- dev/hc-svc-dev.tf
+++ dev/hc-svc-dev.tf
@@ -2,7 +2,7 @@
 //
 module "workspace-hc-svc-dev" {
   source  = "app.terraform.io/funkytown/workspaces/tfe"
-  version = "1.0.15"
+  version = "1.0.20"
 
   workspace_name    = "hc-svc-dev"
   org               = var.org
