51108af8b4be07195c4a0ac2481bd82ee6c5c232
Nick Schofield <nick.schofield@westpac.co.nz>
2021-05-11 21:35:42 +1200

    Vars needed for network workspace config

--- dev/hc-svc-dev.tf
+++ dev/hc-svc-dev.tf
@@ -121,6 +121,29 @@ module "varGithubRepoIdentifier-hc-svc-dev" {
   workspace_id = module.workspace-hc-svc-dev.tw-tw-main-id
   sensitive    = true
 }
+
+module "varOrganisation-hc-svc-dev" {
+  source  = "app.terraform.io/funkytown/vars/tfe"
+  version = "1.0.14"
+
+  key          = "org"
+  value        = var.ORGANISATION
+  category     = "env"
+  workspace_id = module.workspace-hc-svc-dev.tw-tw-main-id
+  sensitive    = true
+}
+
+module "varOrganisation-hc-svc-dev" {
+  source  = "app.terraform.io/funkytown/vars/tfe"
+  version = "1.0.14"
+
+  key          = "resource_group_workspace"
+  value        = var.RESOURCE_GROUP_WORKSPACE
+  category     = "env"
+  workspace_id = module.workspace-hc-svc-dev.tw-tw-main-id
+  sensitive    = true
+}
+
 //// GCP creds
 //
 # module "varGoogleCredentials-hc-svc-dev" {
