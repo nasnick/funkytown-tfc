0cd1b118a7977764df4c851ee8faaa65e8c3e596
Nick Schofield <nick.schofield@westpac.co.nz>
2021-05-11 14:01:52 +1200

    Bump to Workspace version that support VCS settings

--- dev/hc-svc-dev.tf
+++ dev/hc-svc-dev.tf
@@ -2,7 +2,7 @@
 //
 module "workspace-hc-svc-dev" {
   source  = "app.terraform.io/funkytown/workspaces/tfe"
-  version = "1.0.15"
+  version = "1.0.18"
 
   workspace_name    = "hc-svc-dev"
   org               = var.org
